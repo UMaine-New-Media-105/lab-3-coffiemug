1e9e38ea0f7296b43a6d90df4fd2f82d5def7932
coffiemug <124199428+coffiemug@users.noreply.github.com>
2023-02-15 14:19:32 -0500

    move custom shape and if/else to custom functions
    https://editor.p5js.org/coffiemug/full/IbcSixH2q

---
+++ charlieex2functions.js
@@ -0,0 +1,45 @@
+function setup() {
+  createCanvas(400, 400);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(220);
+  
+  flowerCenter(200,130)
+    
+  
+if (mouseX <=100){
+  fill('cornflowerblue');
+  }
+else if(mouseX>300){
+  fill('gold');
+} 
+else{
+  fill('hsl(5,90%,60%)');
+}
+  flower(200,130);
+
+}
+
+function flower(x,y){
+  push();
+  noStroke();
+  translate(x,y)
+ // fill('hsl(5,90%,60%)');
+  ellipse(0,0,90);
+  ellipse(65,50,90);
+  ellipse(40,125,90);
+  ellipse(-40,125,90);
+  ellipse(-65,50,90);
+  pop();
+}
+
+function flowerCenter(x,y){
+  push();
+  translate(x,y)
+  fill('black');
+  ellipse(0,70,80)
+  pop();
+  
+}
